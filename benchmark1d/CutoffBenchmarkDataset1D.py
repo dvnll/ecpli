5dfe504ef78eed8fca6a23dc4b381000e049ef73
residualsilence <spengler.gerrit@gmail.com>
2021-02-21 16:35:46 +0100

    cleanup

--- benchmark1d/CutoffBenchmarkDataset1D.py
+++ benchmark1d/CutoffBenchmarkDataset1D.py
@@ -15,27 +15,42 @@ from ecpli.ECPLiBase import mCrab
 
 
 class CutoffBenchmarkDataset1D(object):
+    """Dataset to benchmark 1-dimensional source analyses.
 
-    def __init__(self, lambda_true, index_true, normalization_true, irf_file):
+        Attributes:
+            true_model: True source model.
+            fit_start_model: Collection of start models (type Models) to
+                             initialize the likelihood fit.
+    """
+
+    def __init__(self, lambda_true: u.quantity.Quantity,
+                 index_true: float,
+                 normalization_true: u.quantity.Quantity,
+                 livetime: u.quantity.Quantity,
+                 pointing_galactic: dict,
+                 e_reco_binning: dict,
+                 on_region_radius: str,
+                 irf_file: str):
 
         normalization_true = normalization_true.to("cm-2 s-1 TeV-1")
         self.lambda_true = lambda_true
         self.index_true = index_true
         self.normalization_true = normalization_true
 
-        livetime = 10 * u.h
-        pointing = SkyCoord(0, 0, unit="deg", frame="galactic")
+        pointing_l = pointing_galactic["pointing_l"]
+        pointing_b = pointing_galactic["pointing_b"]
+        pointing = SkyCoord(pointing_l, pointing_b,
+                            unit="deg", frame="galactic")
 
-        e_reco_min = 0.3
-        e_reco_max = 150.
-
+        e_reco_min = e_reco_binning["e_reco_min"]
+        e_reco_max = e_reco_binning["e_reco_max"]
+        n_e_reco = e_reco_binning["n_e_reco"]
         self.energy_axis = np.logspace(np.log10(e_reco_min),
-                                       np.log10(e_reco_max), 10) * u.TeV
-        
-        on_region_radius = Angle("0.11 deg")
+                                       np.log10(e_reco_max), n_e_reco) * u.TeV
+
+        on_region_radius = Angle(on_region_radius)
         self.on_region = CircleSkyRegion(center=pointing,
                                          radius=on_region_radius)
-
 
         irfs = load_cta_irfs(irf_file)
 
@@ -44,21 +59,21 @@ class CutoffBenchmarkDataset1D(object):
                                       irfs=irfs)
 
     @property
-    def true_model(self):
+    def true_model(self) -> SkyModel:
         model_simu = ExpCutoffPowerLawSpectralModel(
             index=self.index_true,
             amplitude=self.normalization_true,
             lambda_=self.lambda_true,
             reference=1 * u.TeV,
         )
-    
+
         model = SkyModel(spectral_model=model_simu,
                          name="source")
-    
+
         return model
 
     @property
-    def fit_start_model(self):
+    def fit_start_model(self) -> Models:
 
         model_fit = ExpCutoffPowerLawSpectralModel(
                 index=2.2,
@@ -72,8 +87,9 @@ class CutoffBenchmarkDataset1D(object):
 
         return Models([fit_start_model, ])
 
-
-    def data(self):
+    def data(self) -> SpectrumDataset:
+        """Actual event data in form of a SpectrumDataset.
+        """
 
         dataset_empty = SpectrumDataset.create(
                 e_reco=self.energy_axis,
@@ -87,17 +103,20 @@ class CutoffBenchmarkDataset1D(object):
         dataset = maker.run(dataset_empty, self.obs)
         dataset.models = self.true_model
         dataset.fake()
-
         return dataset
 
 
 if __name__ == "__main__":
+    """Main method to create a set of 1-dimensional Monte-Carlo 
+       data (SpectrumDataset) for a given set of true source parameters.
+    """
 
     import argparse
     import pickle
     import string
     import random
     import os
+    import json
 
     letters = string.ascii_uppercase + string.digits
 
@@ -133,31 +152,27 @@ if __name__ == "__main__":
                         dest="IRF",
                         required=True)
 
-    parser.add_argument("--ecut_true_min",
-                        type=float,
-                        help="Minimal cutoff energy in TeV",
-                        dest="ECUT_MIN",
-                        default=5.)
-
-    parser.add_argument("--ecut_true_max",
-                        type=float,
-                        help="Maximal cutoff energy in TeV",
-                        dest="ECUT_MAX",
-                        default=1000.)
-
-    parser.add_argument("--n_ecut",
-                        type=int,
-                        help="Number of cutoff energies.",
-                        dest="N_ECUT",
-                        default=20)
+    parser.add_argument("--config",
+                        type=str,
+                        help="Config file",
+                        dest="CONFIG",
+                        required=True)
 
     options = parser.parse_args()
 
-    if options.ECUT_MAX < options.ECUT_MIN:
+    with open(options.CONFIG, "r") as fin:
+        config = json.load(fin)
+
+    ecut_binning = config["ecut_binning"]
+    ecut_true_max = ecut_binning["ecut_true_max"]
+    ecut_true_min = ecut_binning["ecut_true_min"]
+    n_ecut = ecut_binning["n_ecut"]
+    if ecut_true_max <= ecut_true_min:
         info = "Maximal cutoff energy must be "
         info += "larger than minimal cutoff energy."
         raise RuntimeError(info)
-    if options.N_ECUT <= 0:
+
+    if n_ecut <= 0:
         info = "Number of cutoff energies must be positive."
         raise RuntimeError(info)
 
@@ -172,29 +187,36 @@ if __name__ == "__main__":
 
     n_benchmarks = options.NBENCH
 
-    ecut_true_list = np.logspace(np.log10(options.ECUT_MIN),
-                                 np.log10(options.ECUT_MAX),
-                                 options.N_ECUT) * u.Unit("TeV")
+    ecut_true_list = np.logspace(
+                np.log10(ecut_true_min),
+                np.log10(ecut_true_max),
+                n_ecut) * u.Unit("TeV")
+
     index_true = options.INDEX
     normalization_true = options.NORM * mCrab
 
     for ecut_true in ecut_true_list:
         lambda_true = 1 / ecut_true
         dataset_parameter = {"name": "CutoffBenchmarkDataset1D",
-                             "true_parameter": 
-                                {"lambda_true": lambda_true,
-                                 "index_true": index_true,
-                                 "normalization_true": normalization_true}}
+                             "true_parameter":
+                             {"lambda_true": lambda_true,
+                              "index_true": index_true,
+                              "normalization_true": normalization_true,
+                              "livetime": config["livetime"] * u.h,
+                              "pointing_galactic": config["pointing_galactic"],
+                              "e_reco_binning": config["e_reco_binning"],
+                              "on_region_radius": config["on_region_radius"]}}
 
         for _ in range(n_benchmarks):
-            dataset = CutoffBenchmarkDataset1D(irf_file=options.IRF,
+            dataset = CutoffBenchmarkDataset1D(
+                                    irf_file=options.IRF,
                                     **dataset_parameter["true_parameter"])
             result = {"dataset": dataset}
             result["dataset_parameter"] = dataset_parameter
             result["data"] = dataset.data()
 
             random_string = "".join(random.choice(letters) for _ in range(10))
-
+            dataset.fit_start_model
             _infile = outdir + "benchmark_" + random_string + ".pickle"
             with open(_infile, "wb") as fout:
                 pickle.dump(result, fout)
