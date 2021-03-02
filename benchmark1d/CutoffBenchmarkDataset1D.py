normalization bugfix

benchmark1d/CutoffBenchmarkDataset1D.py
@@ -16,6 +16,7 @@ from gammapy.maps import MapAxis
 from gammapy.irf import load_cta_irfs
 from gammapy.data import Observation
 from ecpli.ECPLiBase import mCrab
+from scipy.optimize import fsolve
 
 
 class CutoffBenchmarkDataset1D(object):
@@ -70,17 +71,34 @@ class CutoffBenchmarkDataset1D(object):
 
     @property
     def true_model(self) -> SkyModel:
+
+        def ecpl_model(amplitude):
+            return ExpCutoffPowerLawSpectralModel(
+                index=self.index_true,
+                amplitude=amplitude / (u.cm**2 * u.s * u.TeV),
+                reference=1 * u.TeV,
+                lambda_=self.lambda_true,
+                alpha=1.0)
+
+        def flux_difference_at_reference(x):
+            diff_norm = self.normalization_true.value
+            flux = ecpl_model(x)(energy=1 * u.TeV)
+            return flux.value - diff_norm
+
+        amplitude = fsolve(flux_difference_at_reference,
+                           self.normalization_true.value)[0]
+
         model_simu = ExpCutoffPowerLawSpectralModel(
             index=self.index_true,
-            amplitude=self.normalization_true,
+            amplitude=amplitude / (u.cm**2 * u.s * u.TeV),
             lambda_=self.lambda_true,
-            reference=1 * u.TeV,
-        )
+            reference=1 * u.TeV)
 
         model = SkyModel(spectral_model=model_simu,
                          name="source")
 
         return model
+
 
     @property
     def fit_start_model(self) -> Models: